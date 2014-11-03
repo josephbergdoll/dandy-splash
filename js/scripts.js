b2e17c222a394332ec93e0d32afd194321247982
Joseph Bergdoll <joseph@osephj.com>
2014-11-03 15:21:15 -0500

    fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,42 @@
 $(document).ready(function() {
-  $('a.link-dandy').hover(function() {
+  $('a.link-dandy').mouseenter(function() {
     if ($('body').hasClass('dandy-clicked')) {
+      if ($('body').hasClass('hoverable')) {
+        $('body').addClass('link-outer-hovered');
+      }
     }
     else {
-      $('body').toggleClass('dandy-hovered');
+      $('body').addClass('dandy-hovered');
+    }
+  });
+
+  $('a.link-dandy').mouseout(function() {
+    if ($('body').hasClass('dandy-clicked')) {
+      if ($('body').hasClass('hoverable')) {
+        $('body').removeClass('link-outer-hovered');
+      }
+      else {
+        $('body').addClass('hoverable');
+      }
+    }
+    else {
+      $('body').removeClass('dandy-hovered');
     }
   });
 
   $('a.link-dandy').click(function(event) {
     event.preventDefault();
-    $('body').toggleClass('dandy-clicked');
+    if ($('body').hasClass('dandy-clicked')) {
+      $('body').removeClass('dandy-clicked');
+      $('body').removeClass('hoverable');
+      $('body').removeClass('link-outer-hovered');
+    }
+    else {
+      $('body').addClass('dandy-clicked');
+      setTimeout(function() {
+        $('body').addClass('hoverable');
+      }, 2000);
+    }
   });
 
   $('a.link-outer').hover(function() {
